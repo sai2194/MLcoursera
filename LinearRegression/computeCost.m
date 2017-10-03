142d47b202cda6b23d9da9ad79cde30219885d9b
Sai Harish Avula <harish02061995@gmail.com>
2017-10-03 21:59:23 +0530

    Update computeCost.m

--- LinearRegression/computeCost.m
+++ LinearRegression/computeCost.m
@@ -10,14 +10,4 @@ sqErrors = (predictions-y).^2;
 % You need to return the following variables correctly 
 J = 1/(2*m)*sum(sqErrors);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
-
-
-
-
-% =========================================================================
-
 end
