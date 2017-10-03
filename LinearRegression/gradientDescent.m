c991cf0f5338314508e9cd4a7d66c67a8f9d1287
Sai Harish Avula <harish02061995@gmail.com>
2017-10-03 22:00:39 +0530

    Update gradientDescent.m

--- LinearRegression/gradientDescent.m
+++ LinearRegression/gradientDescent.m
@@ -9,29 +9,11 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
     pre = X*theta;
     temp = (pre-y);
     temp1 = temp'*X;
-    theta = theta-(alpha/m)*temp1';
-    
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    theta = theta-(alpha/m)*temp1'; 
     J_history(iter) = computeCost(X, y, theta);
 
 end
-
 end
